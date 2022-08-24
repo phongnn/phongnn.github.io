2e381f61374c69bf4a724ee1f1dc03713bc0cfef
Phong Nguyen <phongnn@gmail.com>
2022-08-24 18:00:08 +0700

    Original template

---
+++ astro.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	site: "https://phongnn.github.io",
+	integrations: [
+		mdx({}),
+		tailwind({
+			config: { applyBaseStyles: false },
+		}),
+		image(),
+		sitemap(),
+	],
+});
