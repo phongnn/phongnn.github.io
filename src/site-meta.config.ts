d614488e878c075ff7588eb2878d4882c4d9117f
Phong Nguyen <phongnn@gmail.com>
2022-08-24 21:26:27 +0700

    Update site's meta data

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,11 +1,10 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
-  lang: "en-GB",
-  ogLocale: "en_GB",
-  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
-  themeColorLight: "#fafafa",
-  themeColorDark: "#1d1f21",
-}
+	title: "Phong Nguyen -  Software Developer",
+	description: "Phong Nguyen's personal website",
+	lang: "en-GB",
+	ogLocale: "en_GB",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
 
 export default siteMeta;
